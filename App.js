c19d5e289544306d904f99e8381a5c80bad2d1a3
Pareen <pareenv@gmail.com>
2024-09-10 14:36:43 +0200

    Feat: Added new screen logic

--- App.js
+++ App.js
@@ -1,11 +1,32 @@
-import { StyleSheet, View } from "react-native";
+import { useState } from "react";
+import { ImageBackground, StyleSheet } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import StartGameScreen from "./screens/StartGameScreen";
+import GameScreen from "./screens/GameScreen";
 
 export default function App() {
+  const [userNumber, setUserNumber] = useState();
+
+  function pickedNumberHandler(pickedNumber) {
+    setUserNumber(pickedNumber);
+  }
+
+  let screen = <StartGameScreen pickedNumberHandler={pickedNumberHandler} />;
+
+  if (userNumber) {
+    screen = <GameScreen />;
+  }
+
   return (
     <LinearGradient style={styles.rootScreen} colors={["#f2f1d3", "#f5f171"]}>
-      <StartGameScreen />
+      <ImageBackground
+        source={require("./assets/images/background.png")}
+        resizeMode="cover"
+        style={styles.rootScreen}
+        imageStyle={{ opacity: 0.1 }}
+      >
+        {screen}
+      </ImageBackground>
     </LinearGradient>
   );
 }
--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,11 +1,26 @@
-import { TextInput, View } from "react-native";
+import { Text, StyleSheet, View } from "react-native";
 
 function GameScreen() {
   return (
-    <View>
-      <TextInput />
+    <View style={styles.screen}>
+      <Text>Game Screen!</Text>
+      <View>
+        <Text>Higher or Lower?</Text>
+      </View>
+      <View>
+        <Text>LOG ROUNDS</Text>
+      </View>
     </View>
   );
 }
 
 export default GameScreen;
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 12,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
